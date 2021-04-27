18b17ad52cbd9fad0496becd9b8a02f84ac5d8aa
JatinSeth <79426741+JatinSeth@users.noreply.github.com>
2021-04-28 04:37:02 +0530

    Update script.js
    ud

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ document.getElementById('vibrate').addEventListener('click',vibrate);
 
 function vibrate(){
     document.getElementById('vibrate').innerHTML='Zzz';
-    let vib =navigator.vibrate;
-    vib(1000);
-    vib([1000,500,2000]);
+    // heartbeat pattern (short vibration (100), short pause(20), longer pause(500))
+    window.navigator.vibrate(100,20,100,500);
+    
+//     let vib =navigator.vibrate;
+//     vib(1000);
+//     vib([1000,500,2000]);
 setTimeout(() => {
     document.getElementById('vibrate').innerHTML='click';
 }, 3000);
