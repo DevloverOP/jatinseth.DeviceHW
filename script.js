afa1d611fc51a07d525d1636483faa40918c4516
JatinSeth <79426741+JatinSeth@users.noreply.github.com>
2021-04-28 05:04:14 +0530

    Add files via upload
    new

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-document.getElementById('vibrate').addEventListener('click',vibrate);
+document.getElementById('vibrate').addEventListener('click',buzz);
 
 
+function buzz(){
 
 
-function vibrate(){
-    document.getElementById('vibrate').innerHTML='Zzz';
-    // heartbeat pattern (short vibration (100), short pause(20), longer pause(500))
-     window.navigator.vibrate(1000,20,1000,500);
-    
-//     let vib =navigator.vibrate;
-//     vib(1000);
-//     vib([1000,500,2000]);
-setTimeout(() => {
-    document.getElementById('vibrate').innerHTML='click';
-}, 3000);
-}
+    document.getElementById('vibrate').innerHTML='shoot';
+ 
+ // heartbeat pattern (short vibration (100), short pause(20), longer pause(500))
+ window.navigator.vibrate(1000,20,1000,500);
+ 
+    setTimeout(() => {
+     document.getElementById('vibrate').innerHTML='stop';
+     }, 3000);
+
+     navigator.getUserMedia(
+        // constraints
+        {
+          video: true,
+          audio: true
+        },
+        //successCallback
+        function(localMediaStream) {
+          // select the 'video' object on the DOM
+          var video = document.querySelector('video');
+          video.src = window.URL.createObjectURL(localMediaStream);
+          video.onloadedmetadata = function(e) {
+            // let the video magic begin
+          }
+        },
+        // error!
+        function(err) {
+          console.log("Oh noes, there was the following error:" + err);
+        }
+      );
+
+ }
+ 
+
+
